a34fbc7a039b6e3b24d940f345bf8f34ce04fced
momise92 <boss973@hotmail.fr>
2017-05-31 17:41:06 +0200

    modification de tout le code, aucune erreur

--- login.php
+++ login.php
@@ -1,22 +1,51 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+<?php
+session_start();
+require 'db.php';
+if(isset($_POST['connecter'])){
+	$emailconnect = htmlspecialchars($_POST['emailconnect']);
+	$passconnect = sha1($_POST['passconnect']);
+	if(!empty($emailconnect) && !empty($passconnect)){
+		$req = $db->prepare("SELECT * FROM membre where email = '$emailconnect' AND pass = '$passconnect'");
+		$req->execute(array($emailconnect, $passconnect));
+		$resultat = $req->rowCount();
+		if($resultat == 1){
+			$userinfo = $req->fetch();
+    		$_SESSION['id'] = $userinfo['id'];
+			$_SESSION['pseudo'] = $userinfo['pseudo'];
+			$_SESSION['email'] = $userinfo['email'];
+			header("location: profil.php?id=".$_SESSION['id']);
+		}
+		else{
+			$erreur = "Mauvais identifiant ou mot de passe.";
+		}
+	}
+	else {
+		$erreur = "Veuillez remplir tous les champs.";
+	}
 
-<head>
-	<title>Connexion</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-	<link href="style.css" rel="stylesheet" type="text/css" />
-</head>
+}
 
-<body>
-	<h1>Connectez vous</h1>
+?>
 
+	<?php require 'include/header.php'; ?>
+	<!--Formulaire de connexion-->
+	<h3>Connexion</h3>
+
+	<!--Affichage des erreurs-->
 	<?php
-// Connexion à la base de données
-try
-{
-    $bdd = new PDO('mysql:host=localhost;dbname=mon_blog', 'root', 'root');
+if(isset($erreur)) { //affiche les texte de la variable $erreur.
+
+	echo '<div class="alert alert-danger">'.$erreur.'</div>';
 }
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
+?>
+		<form action="" method="post">
+			<div class="form-group">
+				<label for="emailconnect"> Votre identifiant: </label>
+				<input type="email" name="emailconnect" id="emailconnect" class="form-control" placeholder="Email">
+				<label for="passconnect"> Mot de passe: </label>
+				<input type="password" name="passconnect" id="passconnect" class="form-control" placeholder="Mot de passe">
+			</div>
+			<button type="submit" name="connecter" class="btn btn-default btn-lg btn-block">Se connecter </button>
+		</form>
+
+		<?php require 'include/footer.php'; ?>
