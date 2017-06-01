76cad42804635822e266a394ebbdeec92b5afbd6
momise92 <boss973@hotmail.fr>
2017-06-01 17:26:41 +0200

    Ajout des requete php, et message html

--- profil.php
+++ profil.php
@@ -1,7 +1,47 @@
-<?php require 'include/header.php'; ?>
-<html>
+<?php
+session_start();
+require 'db.php';
+if(isset($_GET['id']) AND $_GET['id'] > 0){
+	$getid = intval($_GET['id']);
+	$req = $db->prepare('SELECT * FROM membre WHERE id = ?');
+	$req->execute(array($getid));
+	$userinfo = $req->fetch();
 
-<h2>Bienvenu !</h2>
+?>
 
+	<?php require 'include/header.php'; ?>
 
-</html>
+	<h2>Profil de
+		<?php echo $userinfo['pseudo']; ?>
+	</h2>
+	<br />
+	<p>Pseudo :
+		<?php echo $userinfo['pseudo']; ?>
+	</p>
+	<br />
+
+	<p>Votre adresse email est :
+		<?php echo $userinfo['email']; ?>
+	</p>
+	<br />
+
+	<p>Vous êtes inscrit depuis le :
+		<?php echo $userinfo['date_inscription']; ?>
+	</p>
+	<br />
+	<?php
+		if(isset($_SESSION['id']) AND $userinfo['id'] == $_SESSION['id'])
+		{?>
+		<a href="#">Editer mon profil.</a>
+		<a href="logout.php">Se déconnecter.</a>
+		<?php
+	}
+	?>
+
+		<?php
+}
+else{
+	echo 'Erreur';
+}
+?>
+			<?php require 'include/footer.php'; ?>
