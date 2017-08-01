51f2b486c80ec85fc0439645b09c4fc40da32988
momise92 <boss973@hotmail.fr>
2017-08-01 03:44:41 +0200

    Correction des indentations

--- profil.php
+++ profil.php
@@ -1,47 +1,45 @@
 <?php
 session_start();
 require 'db.php';
-if(isset($_GET['id']) AND $_GET['id'] > 0){
-	$getid = intval($_GET['id']);
-	$req = $db->prepare('SELECT * FROM membre WHERE id = ?');
-	$req->execute(array($getid));
-	$userinfo = $req->fetch();
+if (isset($_GET['id']) AND $_GET['id'] > 0) {
+    $getid = intval($_GET['id']);
+    $req = $db->prepare('SELECT * FROM membre WHERE id = ?');
+    $req->execute(array($getid));
+    $userinfo = $req->fetch();
 
 ?>
 
-	<?php require 'include/header.php'; ?>
+    <?php require 'include/header.php'; ?>
 
-	<h2>Profil de
-		<?php echo $userinfo['pseudo']; ?>
-	</h2>
-	<br />
-	<p>Pseudo :
-		<?php echo $userinfo['pseudo']; ?>
-	</p>
-	<br />
+    <h2>Profil de
+        <?php echo $userinfo['pseudo']; ?>
+    </h2>
+    <br />
+    <p>Pseudo :
+        <?php echo $userinfo['pseudo']; ?>
+    </p>
+    <br />
 
-	<p>Votre adresse email est :
-		<?php echo $userinfo['email']; ?>
-	</p>
-	<br />
+    <p>Votre adresse email est :
+        <?php echo $userinfo['email']; ?>
+    </p>
+    <br />
 
-	<p>Vous êtes inscrit depuis le :
-		<?php echo $userinfo['date_inscription']; ?>
-	</p>
-	<br />
-	<?php
-		if(isset($_SESSION['id']) AND $userinfo['id'] == $_SESSION['id'])
-		{?>
-		<a href="#">Editer mon profil.</a>
-		<a href="logout.php">Se déconnecter.</a>
-		<?php
-	}
-	?>
+    <p>Vous êtes inscrit depuis le :
+        <?php echo $userinfo['date_inscription']; ?>
+    </p>
+    <br />
+    <?php
+    if (isset($_SESSION['id']) AND $userinfo['id'] == $_SESSION['id']) {?>
+        <a href="#">Editer mon profil.</a>
+        <a href="logout.php">Se déconnecter.</a>
+        <?php
+    }
+    ?>
 
-		<?php
-}
-else{
-	echo 'Erreur';
+        <?php
+} else {
+    echo 'Erreur';
 }
 ?>
-			<?php require 'include/footer.php'; ?>
+            <?php require 'include/footer.php'; ?>
